fc0758869078eae5080092887c2a6b713aa65b5e
abacon118 <ambscout@gmail.com>
2025-04-27 23:36:55 -0400

    IQ_gnuradio

---
+++ SD2_Spring25/Computer/eclypse-z7-notes/projects/sdr_transceiver/gnuradio/QR_Eclypse_IQ.py
@@ -0,0 +1,215 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+#
+# SPDX-License-Identifier: GPL-3.0
+#
+# GNU Radio Python Flow Graph
+# Title: Not titled yet
+# Author: andrew
+# GNU Radio version: 3.10.9.2
+
+from PyQt5 import Qt
+from gnuradio import qtgui
+from PyQt5 import QtCore
+from gnuradio import analog
+from gnuradio import audio
+from gnuradio import blocks
+from gnuradio import filter
+from gnuradio.filter import firdes
+from gnuradio import gr
+from gnuradio.fft import window
+import sys
+import signal
+from PyQt5 import Qt
+from argparse import ArgumentParser
+from gnuradio.eng_arg import eng_float, intx
+from gnuradio import eng_notation
+import eclypse_z7_qr
+import eclypse_z7_qr_rffe
+import sip
+
+
+
+class QR_Eclypse_IQ(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Not titled yet", catch_exceptions=True)
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Not titled yet")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except BaseException as exc:
+            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "QR_Eclypse_IQ")
+
+        try:
+            geometry = self.settings.value("geometry")
+            if geometry:
+                self.restoreGeometry(geometry)
+        except BaseException as exc:
+            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 48000
+        self.rx_samp_rate = rx_samp_rate = 768000
+        self.rx_freq = rx_freq = 103700000
+        self.rx_baseband = rx_baseband = 47000000
+        self.addr = addr = "192.168.49.70"
+
+        ##################################################
+        # Blocks
+        ##################################################
+
+        self._rx_freq_range = qtgui.Range(80000000, 120000000, 100000, 103700000, 200)
+        self._rx_freq_win = qtgui.RangeWidget(self._rx_freq_range, self.set_rx_freq, "Frequency (Hz)", "counter_slider", int, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._rx_freq_win)
+        self._rx_baseband_range = qtgui.Range(1000000, 80000000, 100000, 47000000, 200)
+        self._rx_baseband_win = qtgui.RangeWidget(self._rx_baseband_range, self.set_rx_baseband, "Frequency (Hz)", "counter_slider", int, QtCore.Qt.Horizontal)
+        self.top_layout.addWidget(self._rx_baseband_win)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_fff(
+                interpolation=1,
+                decimation=16,
+                taps=[],
+                fractional_bw=0)
+        self.qtgui_sink_x_0 = qtgui.sink_c(
+            1024, #fftsize
+            window.WIN_BLACKMAN_hARRIS, #wintype
+            (rx_freq*1000000), #fc
+            rx_samp_rate, #bw
+            '', #name
+            True, #plotfreq
+            True, #plotwaterfall
+            True, #plottime
+            True, #plotconst
+            None # parent
+        )
+        self.qtgui_sink_x_0.set_update_time(1.0/10)
+        self._qtgui_sink_x_0_win = sip.wrapinstance(self.qtgui_sink_x_0.qwidget(), Qt.QWidget)
+
+        self.qtgui_sink_x_0.enable_rf_freq(False)
+
+        self.top_layout.addWidget(self._qtgui_sink_x_0_win)
+        self.low_pass_filter_0 = filter.fir_filter_ccf(
+            1,
+            firdes.low_pass(
+                1,
+                rx_samp_rate,
+                90000,
+                10000,
+                window.WIN_HAMMING,
+                6.76))
+        self.eclypse_z7_qr_source_0_0 = eclypse_z7_qr.source(addr, 1002, rx_freq, rx_samp_rate, 0)
+        self.eclypse_z7_qr_source_0 = eclypse_z7_qr.source(addr, 1001, rx_freq, rx_samp_rate, 0)
+        self.eclypse_z7_qr_rffe_0 = eclypse_z7_qr_rffe.rffe(addr, 1000, rx_freq, 0)
+        self.blocks_float_to_complex_0 = blocks.float_to_complex(1)
+        self.audio_sink_0 = audio.sink(48000, '', True)
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=rx_samp_rate,
+        	audio_decimation=1,
+        )
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_wfm_rcv_0, 0), (self.rational_resampler_xxx_0, 0))
+        self.connect((self.blocks_float_to_complex_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.blocks_float_to_complex_0, 0), (self.qtgui_sink_x_0, 0))
+        self.connect((self.eclypse_z7_qr_source_0, 0), (self.blocks_float_to_complex_0, 0))
+        self.connect((self.eclypse_z7_qr_source_0_0, 0), (self.blocks_float_to_complex_0, 1))
+        self.connect((self.low_pass_filter_0, 0), (self.analog_wfm_rcv_0, 0))
+        self.connect((self.rational_resampler_xxx_0, 0), (self.audio_sink_0, 0))
+
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "QR_Eclypse_IQ")
+        self.settings.setValue("geometry", self.saveGeometry())
+        self.stop()
+        self.wait()
+
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+
+    def get_rx_samp_rate(self):
+        return self.rx_samp_rate
+
+    def set_rx_samp_rate(self, rx_samp_rate):
+        self.rx_samp_rate = rx_samp_rate
+        self.eclypse_z7_qr_source_0.set_rate(self.rx_samp_rate)
+        self.eclypse_z7_qr_source_0_0.set_rate(self.rx_samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.rx_samp_rate, 90000, 10000, window.WIN_HAMMING, 6.76))
+        self.qtgui_sink_x_0.set_frequency_range((self.rx_freq*1000000), self.rx_samp_rate)
+
+    def get_rx_freq(self):
+        return self.rx_freq
+
+    def set_rx_freq(self, rx_freq):
+        self.rx_freq = rx_freq
+        self.eclypse_z7_qr_rffe_0.set_freq(self.rx_freq, 0)
+        self.eclypse_z7_qr_source_0.set_freq(self.rx_freq, 0)
+        self.eclypse_z7_qr_source_0_0.set_freq(self.rx_freq, 0)
+        self.qtgui_sink_x_0.set_frequency_range((self.rx_freq*1000000), self.rx_samp_rate)
+
+    def get_rx_baseband(self):
+        return self.rx_baseband
+
+    def set_rx_baseband(self, rx_baseband):
+        self.rx_baseband = rx_baseband
+
+    def get_addr(self):
+        return self.addr
+
+    def set_addr(self, addr):
+        self.addr = addr
+
+
+
+
+def main(top_block_cls=QR_Eclypse_IQ, options=None):
+
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+
+    tb.start()
+
+    tb.show()
+
+    def sig_handler(sig=None, frame=None):
+        tb.stop()
+        tb.wait()
+
+        Qt.QApplication.quit()
+
+    signal.signal(signal.SIGINT, sig_handler)
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    timer = Qt.QTimer()
+    timer.start(500)
+    timer.timeout.connect(lambda: None)
+
+    qapp.exec_()
+
+if __name__ == '__main__':
+    main()
--- SD2_Spring25/Computer/eclypse-z7-notes/projects/sdr_transceiver/gnuradio/eclypse_z7_qr.py
+++ SD2_Spring25/Computer/eclypse-z7-notes/projects/sdr_transceiver/gnuradio/eclypse_z7_qr.py
@@ -7,16 +7,16 @@ import socket
 from gnuradio import gr, blocks
 
 class source(gr.hier_block2):
-  '''Eclypse Z7 QR Source'''
+  '''Eclypse Z7 Source'''
 
   rates = {24000:0, 48000:1, 96000:2, 192000:3, 384000:4, 768000:5, 1536000:6}
 
-  def __init__(self, addr, port, baseband, rate, corr):
+  def __init__(self, addr, port, freq, rate, corr):
     gr.hier_block2.__init__(
       self,
-      name = "eclypse_z7_qr_source",
+      name = "eclypse_z7_source",
       input_signature = gr.io_signature(0, 0, 0),
-      output_signature = gr.io_signature(1, 1, gr.sizeof_gr_complex)
+      output_signature = gr.io_signature(1, 1, gr.sizeof_float)
     )
     self.ctrl_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.ctrl_sock.connect((addr, port))
@@ -25,14 +25,16 @@ class source(gr.hier_block2):
     self.data_sock.connect((addr, port))
     self.data_sock.send(struct.pack('<I', 1))
     fd = os.dup(self.data_sock.fileno())
-    self.connect(blocks.file_descriptor_source(gr.sizeof_gr_complex, fd), self)
-    self.set_freq(baseband, corr)
+    self.connect(blocks.file_descriptor_source(gr.sizeof_float, fd), self)
+    self.set_freq(freq, corr)
     self.set_rate(rate)
 
-  def set_freq_baseband(self, freq, corr):
-    packed_value = (1 << 29) | (0 << 28) | (int((1.0 + 1e-6 * corr) * freq) & 0x0FFFFFFF)
-    self.ctrl_sock.send(struct.pack('<I', packed_value))
-  
+  def set_freq(self, freq, corr):
+    # Calculate frequency as a float and print it
+    corrected_freq = (1.0 + 1e-6 * corr) * freq
+    print(f"Source Frequency (as float): {corrected_freq}")
+    self.ctrl_sock.send(struct.pack('<I', 0<<28 | int(corrected_freq)))
+
   def set_rate(self, rate):
     if rate in source.rates:
       code = source.rates[rate]
@@ -48,8 +50,8 @@ class sink(gr.hier_block2):
   def __init__(self, addr, port, freq, rate, corr, ptt):
     gr.hier_block2.__init__(
       self,
-      name = "eclypse_z7_qr_sink",
-      input_signature = gr.io_signature(1, 1, gr.sizeof_gr_complex),
+      name = "eclypse_z7_sink",
+      input_signature = gr.io_signature(1, 1, gr.sizeof_float),
       output_signature = gr.io_signature(0, 0, 0)
     )
     self.ctrl_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -59,8 +61,8 @@ class sink(gr.hier_block2):
     self.data_sock.connect((addr, port))
     self.data_sock.send(struct.pack('<I', 3))
     fd = os.dup(self.data_sock.fileno())
-    self.null_sink = blocks.null_sink(gr.sizeof_gr_complex)
-    self.file_sink = blocks.file_descriptor_sink(gr.sizeof_gr_complex, fd)
+    self.null_sink = blocks.null_sink(gr.sizeof_float)
+    self.file_sink = blocks.file_descriptor_sink(gr.sizeof_float, fd)
     self.set_freq(freq, corr)
     self.set_rate(rate)
     if ptt:
@@ -73,7 +75,10 @@ class sink(gr.hier_block2):
       self.connect(self, self.null_sink)
 
   def set_freq(self, freq, corr):
-    self.ctrl_sock.send(struct.pack('<I', 0<<28 | int((1.0 + 1e-6 * corr) * freq)))
+    # Calculate frequency as a float and print it
+    corrected_freq = (1.0 + 1e-6 * corr) * freq
+    print(f"Sink Frequency (as float): {corrected_freq}")
+    self.ctrl_sock.send(struct.pack('<I', 0<<28 | int(corrected_freq)))
 
   def set_rate(self, rate):
     if rate in sink.rates:
@@ -97,27 +102,3 @@ class sink(gr.hier_block2):
       self.disconnect(self, self.file_sink)
       self.connect(self, self.null_sink)
       self.unlock()
-
-class rffe(gr.hier_block2):
-  '''Eclypse Z7 Qorvo Radio RF Front End Control'''
-
-  def __init__(self, addr, port, freq, corr):
-    gr.hier_block2.__init__(
-      self,
-      name = "eclypse_z7_qr_rffe",
-      input_signature = gr.io_signature(0, 0, 0),
-      output_signature = gr.io_signature(1, 1, gr.sizeof_gr_complex)
-    )
-    self.ctrl_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.ctrl_sock.connect((addr, port))
-    self.ctrl_sock.send(struct.pack('<I', 0))
-    self.data_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.data_sock.connect((addr, port))
-    self.data_sock.send(struct.pack('<I', 1))
-    fd = os.dup(self.data_sock.fileno())
-    self.connect(blocks.file_descriptor_source(gr.sizeof_gr_complex, fd), self)
-    self.set_freq_rffe(freq, corr)
-
-  def set_freq_rffe(self, freq, corr):
-    packed_value = (0 << 29) | (0 << 28) | (int((1.0 + 1e-6 * corr) * freq) & 0x0FFFFFFF)
-    self.ctrl_sock.send(struct.pack('<I', packed_value))
